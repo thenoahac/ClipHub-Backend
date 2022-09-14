9d107a7c968bffc8920a7c93e069278373fc6c32
Noah Carralero <nole2kgames@gmail.com>
2022-09-14 01:40:36 -0400

    edited index file

--- models/index.js
+++ models/index.js
@@ -1,53 +1,53 @@
-const Appointment = require('./Appointment');
-const Customer = require('./Customer');
-// const Service = require('./Service');
-// const Barber = require('./Barber');
-// const Image = require("./Image");
+// const Appointment = require('./Appointment');
+// const Customer = require('./Customer');
+// // const Service = require('./Service');
+// // const Barber = require('./Barber');
+// // const Image = require("./Image");
 
-// Associations
-// Barber.hasMany(Service, {
-    // foreignKey: 'barber_id'
+// // Associations
+// // Barber.hasMany(Service, {
+//     // foreignKey: 'barber_id'
+// // });
+
+// // Barber.hasMany(Appointment, {
+// //     // foreignKey: 'barber_id'
+// //     constraints: false
+// // });
+
+// // Appointment.belongsTo(Barber, {
+// //     // foreignKey: 'barber_id'
+// //     constraints: false
+// // })
+
+// // Service.belongsTo(Barber, {
+//     // foreignKey: 'barber_id'
+// // });
+
+// Customer.hasOne(Appointment, {
+//     // foreignKey: 'customer_id'
+//     // constraints: false
 // });
 
-// Barber.hasMany(Appointment, {
-//     // foreignKey: 'barber_id'
-//     constraints: false
-// });
-
-// Appointment.belongsTo(Barber, {
-//     // foreignKey: 'barber_id'
+// Appointment.belongsTo(Customer, {
+//     // foreignKey: 'customer_id'
 //     constraints: false
 // })
 
-// Service.belongsTo(Barber, {
-    // foreignKey: 'barber_id'
-// });
+// // Service.hasMany(Appointment, {
+// //     // foreignKey: 'service_id'
+// // });
 
-Customer.hasOne(Appointment, {
-    // foreignKey: 'customer_id'
-    // constraints: false
-});
+// // Appointment.belongsTo(Service, {
+// //     // foreignKey: 'service_id'
+// // });
 
-Appointment.belongsTo(Customer, {
-    // foreignKey: 'customer_id'
-    constraints: false
-})
-
-// Service.hasMany(Appointment, {
-//     // foreignKey: 'service_id'
-// });
-
-// Appointment.belongsTo(Service, {
-//     // foreignKey: 'service_id'
-// });
-
-// Image.belongsTo(Barber, {
-//  //   foreignKey: "u_id",
-//     onDelete: "CASCADE"
-// });
+// // Image.belongsTo(Barber, {
+// //  //   foreignKey: "u_id",
+// //     onDelete: "CASCADE"
+// // });
 
 
-module.exports = {
-    Customer,
-    Appointment
-};
+// module.exports = {
+//     Customer,
+//     Appointment
+// };
