1d98388a7ff11477db6e212c1adeeb0a4c95da95
gio2nice <106710271+gio2nice@users.noreply.github.com>
2022-10-12 23:58:31 -0700

    bb

--- models/Appointment.js
+++ models/Appointment.js
@@ -1,4 +1,4 @@
-const { Model, DataTypes, Sequelize } = require('sequelize');
+const { Model, DataTypes, Sequelize, DATEONLY } = require('sequelize');
 const { sequelize }= require('../config/connection');
 
 class Appointment extends Model { }
